ea5d2047e12126c19aea23e74af5c4a64ce18c8f
Wesley Miranda <wesleywes42@gmail.com>
2022-09-26 12:15:55 -0600

    final

--- spec/requests/api/v1/book/book_spec.rb
+++ spec/requests/api/v1/book/book_spec.rb
@@ -6,5 +6,18 @@ RSpec.describe 'books endpoints' do
     # binding.pry
     response_body = JSON.parse(response.body, symbolize_names: true)
     forecast = response_body[:data]
+    expect(response).to be_successful
+
+    expect(forecast[:id]).to eq("null")
+    expect(forecast[:type]).to eq('books')
+    expect(forecast[:attributes]).to be_a Hash
+    expect(forecast[:attributes][:forecast]).to be_a Hash
+    expect(forecast[:attributes][:forecast][:summary]).to be_a String
+    expect(forecast[:attributes][:forecast][:temperature]).to be_a Float
+
+    expect(forecast[:attributes][:total_books_found]).to be_a Hash
+    expect(forecast[:attributes][:total_books_found][:title]).to be_a String
+    expect(forecast[:attributes][:total_books_found][:publisher]).to be_a String
+
   end
 end
